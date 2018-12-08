2a2f033f501fe2b9f379646a37cea57230137eb5
Sergei <staroduber@yandex.ru>
2018-12-08 17:50:46 +0400

    eth

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.coinmarketcap.com/v1/ticker/ethereum/')
+      .then(response => response.json())
+      .then(data => this.setState({data: data}));
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    let price = this.state.data === null? 'подождите...' : `ETH ${this.state.data[0].price_usd} usd`;
+    return <div>{price}</div>
   }
 }
 
