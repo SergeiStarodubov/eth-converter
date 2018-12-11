d288414bd80cbdc72ced1656add784cfed1e8231
Sergei <staroduber@yandex.ru>
2018-12-11 20:26:14 +0400

    eth-converter

--- src/App.js
+++ src/App.js
@@ -10,22 +10,26 @@ class App extends React.Component {
   componentDidMount() {
     fetch('https://api.coinmarketcap.com/v1/ticker/ethereum/')
       .then(response => response.json())
-      .then(data => this.setState({eth: data}));
+      .then(data => this.setState({eth: data}))
+      .catch(() => {
+        this.setState({rub: 'проблемы с сервером, перезагрузите страницу...'})
+      });
 
     fetch('http://free.currencyconverterapi.com/api/v5/convert?q=USD_RUB&compact=y')
       .then(response => response.json())
       .then(data => {
         this.setState({rub: data});
-      }
-    );
+      }).catch(() => {
+        this.setState({rub: 'проблемы с сервером, перезагрузите страницу...'})
+      });
   }
   render() {
     const eth = this.state.eth === undefined? 'загрузка...' : `ETH ${this.state.eth[0].price_usd} usd`;
-    const usd = this.state.rub === undefined? 'загрузка...' : this.state.rub.USD_RUB.val;  
+    const usd = this.state.rub === undefined? 'загрузка...' : this.state.rub.USD_RUB.val;
     return (
       <>
       <Info eth = {eth} usd = {usd}/>
-      <Panel/>
+      <Panel eth = {this.state.eth === undefined? '' : this.state.eth[0].price_usd} usd = {usd}/>
       </>
     );
   }
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -8,10 +8,12 @@ class Panel extends React.Component {
   }
   state = {
     block: 'none',
-    currency: 'RUB'
+    currency: 'USD',
+    inputETH: '',
+    inputFiat: ''
   }
 
-  chooseCurrency = () => {
+  chooseCurrency = (e) => {
     window.onclick = (e) => {
       if (!e.target.classList.contains('fiat')) {
         this.setState({block: 'none'});
@@ -20,24 +22,70 @@ class Panel extends React.Component {
       }
     }
   }
+
   putFiatInPanel = (e) => {
-    this.setState({currency: e.target.innerHTML});
+    this.state.currency = e.target.innerHTML;
+    this.setState({currency: this.state.currency});
+    window.onclick = (e) => {
+      e.preventDefault();
+    }
+    this.setState({block: 'none'});
+    if (this.state.currency === 'RUB') {
+      this.state.inputFiat = (+this.state.inputETH)*(+this.props.eth* +this.props.usd);
+      this.setState({inputFiat: this.state.inputFiat.toFixed(2)});
+    } else {
+      this.state.inputFiat = (+this.state.inputETH)*(+this.props.eth);
+      this.setState({inputFiat: this.state.inputFiat.toFixed(2)});
+    }
   }
 
-  componentDidMount() {
-
+  handleInputETH = (e) => {
+    if (isNaN(e.target.value) === false) {
+      if (this.state.currency === 'USD') {
+        this.state.inputETH = e.target.value;
+        this.setState({inputETH: this.state.inputETH});
+        this.state.inputFiat = (+this.state.inputETH)*(+this.props.eth);
+        this.setState({inputFiat: this.state.inputFiat.toFixed(2)});
+      } else {
+        this.state.inputETH = e.target.value;
+        this.setState({inputETH: this.state.inputETH});
+        this.state.inputFiat = (+this.state.inputETH)*(+this.props.eth* +this.props.usd);
+        this.setState({inputFiat: this.state.inputFiat.toFixed(2)});
+      }
+    }
   }
+
+  handleInputFiat = (e) => {
+    if (isNaN(e.target.value) === false) {
+      if (this.state.currency === 'USD') {
+        this.state.inputFiat = e.target.value;
+        this.setState({inputFiat: this.state.inputFiat});
+        this.state.inputETH = (+this.state.inputFiat)/(+this.props.eth);
+        this.setState({inputETH: this.state.inputETH.toFixed(5)});
+      } else {
+        this.state.inputFiat = e.target.value;
+        this.setState({inputFiat: this.state.inputFiat});
+        this.state.inputETH = (+this.state.inputFiat)/(+this.props.eth* +this.props.usd);
+        this.setState({inputETH: this.state.inputETH.toFixed(5)});
+      }
+    }
+  }
+
   render() {
 
     return (
       <div className = 'panel'>
+
         <p>
-          <input type="text"/><span>ETH</span> <input type="text"/>
+          <input type="text" value = {this.state.inputETH} onChange = {this.handleInputETH} /><span>ETH</span> <input type="text" onChange = {this.handleInputFiat} value = {this.state.inputFiat} />
           <span className = 'fiat' ref = {this.fiat} onClick = {this.chooseCurrency}>{this.state.currency}</span>
         </p>
-      <div className = 'block fiat' style = {{display: this.state.block}}>
-        <p className = 'fiat' onClick = {this.putFiatInPanel}>USD</p>
-        <p className = 'fiat' onClick = {this.putFiatInPanel}>RUB</p></div>
+
+        <div className = 'block fiat' style = {{display: this.state.block}}>
+          <p className = 'fiat' onClick = {this.putFiatInPanel}>USD</p>
+          <p className = 'fiat' onClick = {this.putFiatInPanel}>RUB</p>
+        </div>
+
       </div>
     );
   }
